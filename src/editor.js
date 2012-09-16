23ec5b668cde5f2f5104e6f43d3f4cb49b23e5c0
rsimon <rainer.simon@ait.ac.at>
2012-09-16 21:14:50 +0200

    More work on editor class

--- src/editor.js
+++ src/editor.js
@@ -1,11 +1,65 @@
 goog.provide('yuma.editor');
 
+goog.require('goog.soy');
+goog.require('goog.dom');
+goog.require('goog.dom.query');
+goog.require('goog.style');
+
 /**
  * Base annotation edit form.
+ * @param {yuma.model.Annotation | undefined} annotation
  * @constructor
  * @extends {goog.events.EventTarget}
  */
-yuma.editor.Editor = function() {
+yuma.editor.Editor = function(annotation) {
+  /** @private **/
+  this._div = goog.soy.renderAsElement(yuma.templates.editform);
 
+  /** @private **/
+  this._textarea = goog.dom.query('.annotation-text', this._div)[0];
+
+  /** @private **/
+  this._btnCancel = goog.dom.query('.annotation-cancel', this._div)[0];
+
+  /** @private **/
+  this._btnSave = goog.dom.query('.annotation-save', this._div)[0];
+
+  yuma.events.EventBroker.getInstance().registerEventTarget(this, [
+    yuma.events.EventType.ANNOTATION_EDIT,
+    yuma.events.EventType.ANNOTATION_EDIT_CANCEL,
+    yuma.events.EventType.ANNOTATION_EDIT_SAVE
+  ]);
+
+  var self = this;
+  goog.events.listen(this._btnCancel, goog.events.EventType.CLICK, function(event) {
+    // TODO dispatch ANNOTATION_EDIT_CANCEL event
+    self.close();
+  });
+
+  goog.events.listen(this._btnSave, goog.events.EventType.CLICK, function(event) {
+    // TODO dispatch ANNOTATION_EDIT_SAVE event
+    self.close();
+  });
+ 
+  goog.dom.appendChild(document.body, this._div);
+  this._textarea.focus();
+
+  // TODO dispatch ANNOTATION_EDIT event
 }
 goog.inherits(yuma.editor.Editor, goog.events.EventTarget);
+
+/**
+ * Sets the position.
+ * @param {number} x the X coordinate
+ * @param {number} y the Y coordinate
+ */
+yuma.editor.Editor.prototype.setPosition = function(x, y) {
+  goog.style.setPosition(this._div, x, y);
+}
+
+/**
+ * Closes the editor.
+ */
+yuma.editor.Editor.prototype.close = function() {
+  goog.dom.removeNode(this._div);
+}
