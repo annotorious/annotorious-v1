b681d0b580c2d0db1c5206ffcb20c28057896a06
Rainer Simon <rainer.simon@ait.ac.at>
2012-09-27 16:18:59 +0200

    Global 'addHandler' method for image module

--- src/modules/image/image_module.js
+++ src/modules/image/image_module.js
@@ -10,8 +10,14 @@ goog.require('goog.events');
  * @constructor
  */
 yuma.modules.image.ImageModule = function() {
+  /** @private **/
   this._allImages = goog.dom.query('img.annotatable', document);
+  
+  /** @private **/
   this._imagesToLoad = goog.array.clone(this._allImages);
+  
+  /** @private **/
+  this._annotators = [];
   
   // Make images in viewport annotatable
   this._lazyLoad();
@@ -33,10 +39,21 @@ yuma.modules.image.ImageModule.prototype._lazyLoad = function() {
   var self = this;
   goog.array.forEach(this._imagesToLoad, function(image, idx, array) {
     if (yuma.modules.image.isInViewport(image)) {
-      new yuma.modules.image.ImageAnnotator(image);
+      self._annotators.push(new yuma.modules.image.ImageAnnotator(image));
       goog.array.remove(self._imagesToLoad, image);
     }
   });  
+}
+
+/**
+ * Adds a lifecycle event handler to the image module.
+ * @param {yuma.events.EventType} type the event type
+ * @param {function} handler the handler function
+ */
+yuma.modules.image.ImageModule.prototype.addHandler = function(type, handler) {
+  goog.array.forEach(this._annotators, function(annotator, idx, array) {
+    annotator.addHandler(type, handler);
+  });
 }
 
 if (typeof window.onload != 'function') {
