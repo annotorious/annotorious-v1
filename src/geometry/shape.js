c48723772f9947966a2675872dd626344c154517
Rainer Simon <rainer.simon@ait.ac.at>
2012-12-15 22:41:25 +0100

    Added missing commit

---
+++ src/geometry/shape.js
@@ -0,0 +1,134 @@
+goog.provide('annotorious.shape');
+
+/**
+ * A shape. Consists of descriptive shape metadata, plus the actual shape geometry.
+ * @param {annotorious.shape.ShapeType} type the shape type
+ * @param {annotorious.shape.geom.Point | annotorious.shape.geom.Rectangle | annotorious.shape.geom.Polygon} geometry the geometry
+ * @param {annotorious.shape.Units} units geometry measurement units
+ * @constructor
+ */
+annotorious.shape.Shape = function(type, geometry, units) {
+  this.type = type
+  this.geometry = geometry;
+  if (units)
+    this.units = units;
+}
+
+/**
+ * Possible shape types
+ * @enum {string}
+ */
+annotorious.shape.ShapeType = {
+  POINT: 'point',
+  RECTANGLE: 'rect',
+  POLYGON: 'polygon'
+}
+
+/**
+ * Possible unit types
+ * @enum {string}
+ */
+annotorious.shape.Units = {
+  PIXEL: 'pixel',
+  FRACTION: 'fraction'
+}
+
+
+
+/** Helper functions **/
+
+
+
+/**
+ * Checks whether a given shape intersects a point.
+ * @param {annotorious.shape.Shape} shape the shape
+ * @param {number} px the X coordinate
+ * @param {nubmer} py the Y coordinate
+ * @returns {boolean} true if the point intersects the shape
+ */
+annotorious.shape.intersects = function(shape, px, py) {
+    if (shape.type == annotorious.shape.ShapeType.RECTANGLE) {
+      if (px < shape.geometry.x)
+        return false;
+
+      if (py < shape.geometry.y)
+        return false;
+
+      if (px > shape.geometry.x + shape.geometry.width)
+        return false;
+
+      if (py > shape.geometry.y + shape.geometry.height)
+        return false;
+    
+      return true;
+    }
+    
+    return false;
+}
+
+/**
+ * Returns the size of a given shape.
+ * @param {annotorious.shape.Shape} shape the shape
+ * @returns {number} the size
+ */
+annotorious.shape.size = function(shape) {
+  if (shape.type == annotorious.shape.ShapeType.RECTANGLE) {
+    return shape.geometry.width * shape.geometry.height;
+  }
+  
+  return 0;
+}
+
+/**
+ * Returns the bounding rectangle of a given shape.
+ * @param {annotorious.shape.Shape} shape the shape
+ * @returns {annotorious.geom.Rectangle} the bounding rectangle
+ */
+annotorious.shape.getBoundingRect = function(shape) {
+  if (shape.type == annotorious.shape.ShapeType.RECTANGLE) {
+    return shape.geometry;
+  } else if (shape.type == annotorious.shape.ShapeType.POLYGON) {
+    var points = shape.geometry.points;
+
+    var left = points[0].x;
+    var right = points[0].x;
+    var top = points[0].y;
+    var bottom = points[0].y;
+
+    for (var i=1; i<points.length; i++) {
+      if (points[i].x > right)
+        right = points[i].x;
+
+      if (points[i].x < left)
+        left = points[i].x;
+
+      if (points[i].y > bottom)
+        bottom = points[i].y;
+
+      if (points[i].y < top)
+        top = points[i].y;
+    }
+
+    return new annotorious.geom.Rectangle(left, top, right - left, bottom - top);
+  } else {
+    return undefined;
+  }
+}
+
+/**
+ * Transforms a shape from a source to a destination coordinate system. The transformation
+ * is calculated using the transformationFn parameter, which must be a function(xy)
+ * that transforms a single XY coordinate.
+ * @param {annotorious.shape.Shape} shape the shape to transform
+ * @param {function} transformationFn the transformation function
+ * @returns {annotorious.shape.Shape} the transformed shape
+ */
+annotorious.shape.transform = function(shape, transformationFn) {
+  if (shape.type == annotorious.shape.ShapeType.RECTANGLE) {
+    var geom = shape.geometry;
+    var anchor = transformationFn({ x: geom.x, y: geom.y });
+    var size = transformationFn({ x: geom.width, y: geom.height });
+    return new annotorious.shape.Shape(annotorious.shape.ShapeType.RECTANGLE, 
+      new annotorious.shape.geom.Rectangle(anchor.x, anchor.y, size.x, size.y));
+  }
+}
