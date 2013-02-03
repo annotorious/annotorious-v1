32f520bc92ce66674e1f55bb205092563fd4ccf9
Rainer Simon <rainer.simon@ait.ac.at>
2013-02-03 16:26:24 +0100

    Refactoring

--- src/hint.js
+++ src/hint.js
@@ -11,7 +11,7 @@ goog.require('goog.dom.query');
 annotorious.hint.Hint = function(annotator, parent) {
   var self = this;
 
-  this.element = goog.soy.renderAsElement(annotorious.templates.hint);
+  this.element = goog.soy.renderAsElement(annotorious.templates.image.hint);
 
   /** @private **/
   this._annotator = annotator;
