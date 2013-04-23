1df140e287e4632410125780176fb350f12754c5
Rainer Simon <rainer.simon@ait.ac.at>
2013-04-23 21:49:10 +0200

    More copy-and-paste in preparation of generalizing module methods

--- src/modules/module.js
+++ src/modules/module.js
@@ -65,3 +65,125 @@ annotorious.modules.Module.prototype.annotatesItem = function(item_url) {
   }
 }
 
+/**
+ * Standard module method: returns all annotations on the annotatable media with the specified
+ * URL, or all annotations from this module in case no URL is specified.
+ * @param {string | undefined} opt_media_url a media URL (optional)
+ * @return {Array.<Annotation>} the annotations
+ */
+annotorious.modules.Module.prototype.getAnnotations = function(opt_item_url) {
+  if (opt_item_url) {
+    var annotator = this._annotators.get(opt_item_url);
+    if (annotator) {
+      return annotator.getAnnotations();
+    } else {
+      return goog.array.filter(this._bufferedForAdding, function(annotation) {
+        return annotation.src == opt_item_url;
+      });
+    }
+  } else {
+    var annotations = [];
+    goog.array.forEach(this._annotators.getValues(), function(annotator) {
+      goog.array.extend(annotations, annotator.getAnnotations());
+    });
+    goog.array.extend(annotations, this._bufferedForAdding);
+    return annotations;
+  }
+}
+
+/**
+ * Returns the list of available shape selectors for a particular item.
+ * @param {string} the URL of the item to query for available selectors
+ * @returns {List.<string>} the list of selector names
+ */
+annotorious.modules.Module.prototype.getAvailableSelectors = function(item_url) {
+  if (this.annotatesItem(item_url)) {
+    var annotator = this._annotators.get(item_url);
+    if (annotator) {
+      return goog.array.map(annotator.getAvailableSelectors(), function(selector) {
+        return selector.getName();
+      });
+    }
+  }
+}
+
+/**
+ * Standard module method: highlights the specified annotation.
+ * @param {Annotation} annotation the annotation
+ */
+annotorious.modules.Module.prototype.highlightAnnotation = function(annotation) {
+  if (annotation) {
+    if (this.annotatesItem(annotation.src)) {
+      var annotator = this._annotators.get(annotation.src);
+      if (annotator)
+        annotator.highlightAnnotation(annotation);
+    }  
+  } else {
+    goog.array.forEach(this._annotators.getValues(), function(annotator) {
+      annotator.highlightAnnotation();
+    });
+  }
+}
+
+/**
+ * Standard module method: Makes an item annotatable, if it is supported by this module.
+ * @param {object} item the annotatable item
+ */
+annotorious.modules.Module.prototype.makeAnnotatable = function(item) {
+  if (this.supports(item)) {
+    this._allItems.push(item);
+    this._initAnnotator(item);
+  }
+}
+
+/**
+ * Standard module method: removes an annotation from the item with the specified src URL.
+ * @param {Annotation} annotation the annotation
+ */
+annotorious.modules.Module.prototype.removeAnnotation = function(annotation) {
+  if (this.annotatesItem(annotation.src)) {
+    var annotator = this._annotators.get(annotation.src);
+    if (annotator)
+      annotator.removeAnnotation(annotation);
+    else
+      this._bufferedForRemoval.push(annotation);
+  }
+}
+
+/**
+ * Standard module method: sets a specific selector on a particular item.
+ * @param {string} the URL of the item on which to set the selector
+ * @param {string} the name of the selector to set on the item
+ */
+annotorious.modules.Module.prototype.setActiveSelector = function(item_url, selector) {
+  if (this.annotatesItem(item_url)) {
+    var annotator = this._annotators.get(item_url);
+    if (annotator)
+      annotator.setActiveSelector(selector);
+  }
+}
+
+/**
+ * Standard module method: enables (or disables) the ability to create new annotations
+ * on an annotatable item.
+ * @param {boolean} enabled if <code>true</code> new annotations can be created
+ */
+annotorious.modules.Module.prototype.setSelectionEnabled = function(enabled) {
+  this._isSelectionEnabled = enabled;
+  goog.array.forEach(this._annotators.getValues(), function(annotator) {
+    annotator.setSelectionEnabled(enabled);
+  });
+}
+
+/**
+ * Standard module method: tests if this module is able to support annotation on the
+ * specified item.
+ * @param {object} item the item to test
+ * @return {boolean} true if this module can provide annotation functionality for the item
+ * @interface
+ */
+annotorious.modules.Module.prototype.supports = function(item) {
+  /** implementation provided by subclass **/
+}
+
+
